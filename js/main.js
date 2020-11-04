11f1a2098b052c16046f1a1ca7392164db19dd9a
Nazim Uddin <umnazim@gmail.com>
2020-11-03 21:34:10 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+// const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
@@ -15,7 +15,7 @@ function toggleMenu() {
     menuBtn.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
-    menuBranding.classList.add('show');
+    // menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
     // Set Menu State
@@ -24,7 +24,7 @@ function toggleMenu() {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
+    // menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
     // Set Menu State
